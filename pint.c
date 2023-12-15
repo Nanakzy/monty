aa6909f584424a8d33cfc2ea73c7973391586dbc
Nana <nanakzy@gmail.com>
2023-12-15 09:38:21 -0800

    task1

--- pint.c
+++ pint.c
@@ -9,10 +9,20 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
+	stack_t *temp;
+
 	if (!stack || !*stack)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
 		exit(1);
 	}
 	printf("%d\n", (*stack)->n);
+
+	temp = *stack;
+	*stack = temp->next;
+	if (*stack)
+	{
+		(*stack)->prev = NULL;
+	}
+	free(temp);
 }
